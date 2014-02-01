7deb13654c12c5a340a1de64f261f7092be3f180
John Morgan <jtmorgan61@gmail.com>
2014-01-31 18:15:35 -0800

    week 1 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    render 'posts/index' #default, unnecessary
+  end
 
+  def show
+    @post = Post.find(params[:id])
+    render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 PostitTemplate::Application.routes.draw do
-  root to: 'posts#index'
+  root to: 'posts#index' #just root is ok here too
+
+
+  resources :posts, except: [:destroy] do
+    resources :comments, only: [:create, :show]
+  end
 end
+
+# stock set of routes, replaced by resources :posts
+  # get '/posts', to: 'posts#index'
+  # get '/posts/:id', to: 'posts#show'
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts', to: 'posts#create'
+  # get '/posts/:id/edit', to: 'posts#edit'
+  # patch '/posts/:id', to: 'posts#update'
+
+# can set controller with: ", controller: 'images'"

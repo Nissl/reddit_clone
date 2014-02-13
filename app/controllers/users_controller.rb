5f4632386408c680d1e161cdb6b836c08159e41f
John Morgan <jtmorgan61@gmail.com>
2014-02-13 14:07:33 -0800

    minor authentication tweaks from solution

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
+  def show; end
 
   def new
     @user = User.new
@@ -15,20 +19,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    #binding.pry
-    @user = User.find(params[:id])
     @user.update_attributes(user_params)
     if @user.save
-      flash[:notice] = "You updated your info, #{params[:user][:username]}!"
+      flash[:notice] = "You updated your info, #{@user.username}!"
       session[:user_id] = @user.id
       redirect_to posts_path
     else 
@@ -40,5 +36,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You're not allowed to do that."
+      redirect_to root_path
+    end
   end
 end
